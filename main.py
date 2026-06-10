28e11b7ab1501c2fca3f7e4f0fe40dff24cdb984
dibikadahal <dibikadahal@gmail.com>
2026-06-10 12:51:18 +0545

    main.py added

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import requests
+import socket
+import time
+import os
+
+# load the .env file
+load_dotenv()
+
+app = FastAPI()
+
+# -----------Layer 1: Networking Inspector-----------
+def get_network_info(url : str):
+    result = {}
+
+    # Step1: Get the domain and IP address
+    domain = url.replace("https://", "").replace("http://", "").split("/")[0]
+    result['domain'] = domain
+    result['ip_address'] = socket.gethostbyname(domain)
+
+    # Step2: HTTP Request (State, speed, server)
+    start = time.time()
+    response = requests.get(url, timeout = 10)
+    end = time.time()
+
+    result["status_code"] = response.status_code
+    result["response_time_ms"] = round((end - start) * 1000)
+    result["server"] = response.headers.get("Server", "Not disclosed")
+    result["https"] = url.startswith("https://")
+    result["content_type"] = response.headers.get("Content-Type", "Unknown")
+
+    return result, response.text
+
+# test endpoint
+@app.get("/")
+def home():
+    return {"message": "Website Intelligence Tool is running!"}"
+
+@app.get("/inspect")
+def inspect(url:str):
+    network_info, _ = get_network_info(url)
+    return {"network_info": network_info}
+    
